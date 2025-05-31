8a19dbae0642c08b1c7ef9c12a2d761132d15f5a
Sphelele-Micah-Ngubane <sphemicah@gmail.com>
2025-05-31 13:15:21 +0200

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,9 +6,9 @@ const DashboardLayout = ({
   return (
     <div className="h-screen flex">
       { /*LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] lg:w-[14%] bg-red-200">L</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">L</div>
       { /*RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] lg:w-[86%] bg-blue-200">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
       {children}
       </div>
     </div>
